e326eb8f12cdd8f4904629ffaeaec59851e03101
Mamelski <jakub.mamelski@outlook.com>
2020-02-26 19:45:36 +0100

    Pass different types as param

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom';
 import Dialog from './Dialog';
 import './index.css';
 
+var number = 7;
+var object = {firstName:"Jakub", lastName:"Mamelski"};
+var array = ["jabłko", "banan"];
+var myFunction = function() {
+  return 1+2;
+}
+
+var html = <span><b>To jest kod html</b></span>;
+
 const element = (
   <div>
-    <Dialog content="Wow, pobrałem tytuł z propsów" />
+    {/* <Dialog content="Wow, pobrałem tytuł z propsów" /> */}
+    {/* <Dialog content={number} /> */}
+    {/* <Dialog content={object} /> */}
+    {/* <Dialog content={array}/> */}
+    {/* <Dialog content={myFunction}/> */}
+    {/* <Dialog content={myFunction()}/> */}
+    <Dialog content={html}/>
   </div>
 );
 
