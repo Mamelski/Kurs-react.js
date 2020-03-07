f9a6e4455e38f10a6cf2c039317794e005955fe6
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-03-07 23:16:10 +0100

    Add contact list

---
+++ my-app/src/ContactList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ContactItem from './ContactItem';
+
+function ContactList() {
+  return (
+    <ul className="ui relaxed divided list selection">
+      <ContactItem
+        login="typeofweb1"
+        name="Lena"
+        department="JavaScript Developer"
+      />
+      <ContactItem
+        login="typeofweb2"
+        name="Brian"
+        department="Human Resources"
+      />
+      <ContactItem
+        login="typeofweb3"
+        name="Rick"
+        department="QA"
+      />
+    </ul>
+  )
+}
+
+export default ContactList;
