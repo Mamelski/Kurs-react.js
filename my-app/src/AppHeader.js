16881a83a8046dfe8c7683307996f105c7704875
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-03-10 22:18:11 +0100

    Change AppHeader to Stateless Functional Component

--- my-app/src/AppHeader.js
+++ my-app/src/AppHeader.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function AppHeader() {
-  return (
-    <header className="ui fixed menu">
-      <nav className="ui container">
-        <a href="#" className="header item">
-          <img className="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
-          Lista kontaktów
+class AppHeader extends Component {
+  render() {
+    return (
+      <header className="ui fixed menu" >
+        <nav className="ui container">
+          <a href="#" className="header item">
+            <img className="logo" src="https://typeofweb.com/wp-content/uploads/2017/08/cropped-typeofweb_logo-04-white-smaller-1-e1504359870362.png" />
+            Lista kontaktów
         </a>
-        <div className="header item">
-          <button className="ui button">Dodaj</button>
-        </div>
-      </nav>
-    </header>
-  )
+          <div className="header item">
+            <button className="ui button">Dodaj</button>
+          </div>
+        </nav>
+      </header>
+    )
+  }
 }
 
 export default AppHeader
