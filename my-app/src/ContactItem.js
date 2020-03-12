b7be456e7e842df1d5757798ce95dfaa6028bd20
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-03-12 22:33:15 +0100

    Update contact item

--- my-app/src/ContactItem.js
+++ my-app/src/ContactItem.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-function ContactItem({ login, name, department }) {
-  const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
-  return (
-    <li className="item">
-      <img src={imgUrl} className="ui mini rounded image" />
-      <div className="content">
-        <h4 className="header">{name}</h4>
-        <div className="description">{department}></div>
-      </div>
-    </li>
-  )
+class ContactItem extends React.Component {
+  render() {
+    const { login, name, department } = this.props
+    const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+    return (
+      <li className="item">
+        <img src={imgUrl} className="ui mini rounded image" />
+        <div className="content">
+          <h4 className="header">{name}</h4>
+          <div className="description">{department}</div>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default ContactItem
