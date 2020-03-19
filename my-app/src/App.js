963484d14324942436fecca097f2c4168f2ece72
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-03-19 22:22:29 +0100

    Change app to class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import AppHeader from './AppHeader';
 import ContactsList from './ContactsList';
 
-function App() {
-  return (
-    <div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"></link>
-      <AppHeader />
-      <main className="ui main text container">
-        <ContactsList />
-      </main>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"></link>
+        <AppHeader />
+        <main className="ui main text container">
+          <ContactsList />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
