2198b04996e65f2b6ad784517add6850c793b615
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-03-22 23:42:48 +0100

    move onClick to method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,26 @@ import './App.css';
 import AppHeader from './AppHeader';
 import ContactsList from './ContactsList';
 
+function onClickHandler() {
+  alert('Kliknięto!');
+}
 
 class App extends Component {
   render() {
-
-    return (
-      <button onClick={() => alert('Kliknięto!')}>Kliknij!</button>
-      // return (
-      //   <div>
-      //     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"></link>
-      //     <AppHeader />
-      //     <main className="ui main text container">
-      //       <ContactsList />
-      //     </main>
-      //   </div>
-    );
+    return <button onClick={this.onClickHandler}>Kliknij!</button>;
+  }
+  onClickHandler() {
+    alert("Kliknięto!");
   }
 }
+// return (
+//   <div>
+//     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"></link>
+//     <AppHeader />
+//     <main className="ui main text container">
+//       <ContactsList />
+//     </main>
+//   </div>
+
 
 export default App;
