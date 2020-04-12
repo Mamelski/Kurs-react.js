f351b688af9cc6bde4f89518d88097dfe45957bb
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-04-12 09:26:08 +0200

    move avatar to separate component

---
+++ my-app/src/Avatar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Avatar extends Component {
+  render() {
+    const { login } = this.props;
+    const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
+    return (
+      <img src={imgUrl} className="ui mini rounded image" />
+    );
+  }
+}
+
+export default Avatar;
--- my-app/src/ContactItem.js
+++ my-app/src/ContactItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Avatar from './Avatar';
 
 class ContactItem extends React.Component {
   render() {
     const { login, name, department } = this.props
-    const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
     return (
       <li className="item">
-        <img src={imgUrl} className="ui mini rounded image" />
+        <Avatar login={login} />
         <div className="content">
           <h4 className="header">{name}</h4>
           <div className="description">{department}</div>
