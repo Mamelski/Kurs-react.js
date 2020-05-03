cc04dbe97ed909c7c9ca27250ba7a3f3889a4fe7
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-05-03 21:02:53 +0200

    Add more events to MyComponent

--- my-app/src/MyComponent.js
+++ my-app/src/MyComponent.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
 class MyComponent extends Component {
+
   render() {
     return (
-        <button onClick={() => this.onEvent('kliknęło')}>Kliknij!</button>
+        <button
+            onClick={() => this.onEvent('on click')}
+            onMouseOver={() =>this.onEvent('on mouse over')}
+            onDragStart={() =>this.onEvent('on drag start')}>Kliknij!</button>
     );
   }
 
-  onEvent(text) {
+  onEvent = text => {
     alert(text);
   };
 }
