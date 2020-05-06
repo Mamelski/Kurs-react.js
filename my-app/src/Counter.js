70726ced7dd677df81d25f195b14f4f51e885f80
Jakub Mamelski <jakub.mamelski@outlook.com>
2020-05-06 22:46:58 +0200

    Add ctor with state

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -1,7 +1,10 @@
 import React, { Component }  from 'react';
 
 class Counter extends Component{
-
+    constructor() {
+        super();
+        this.state = {counter: 0};
+    }
     render() {
         return (
             <div>
